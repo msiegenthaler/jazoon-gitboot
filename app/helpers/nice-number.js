398ae735761e0e93fdf00f679f52931e54ebecac
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2016-04-07 16:21:11 +0200

    Add nice-number helper.

---
+++ app/helpers/nice-number.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export function niceNumber(params/*, hash*/) {
+  const number = params[0];
+  if (number < 1000) {
+    return number;
+  } else {
+    return Math.round(number / 1000) + "K";
+  }
+}
+
+export default Ember.Helper.helper(niceNumber);
---
+++ tests/unit/helpers/nice-number-test.js
@@ -0,0 +1,30 @@
+import { niceNumber } from 'gitboot/helpers/nice-number';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | nice number');
+
+// Replace this with your real tests.
+test('it returns 1 for 1', function(assert) {
+  let result = niceNumber([1]);
+  assert.equal(result, "1");
+});
+test('it returns 14 for 14', function(assert) {
+  let result = niceNumber([14]);
+  assert.equal(result, "14");
+});
+test('it returns 999 for 999', function(assert) {
+  let result = niceNumber([999]);
+  assert.equal(result, "999");
+});
+test('it returns 1K for 1005', function(assert) {
+  let result = niceNumber([1005]);
+  assert.equal(result, "1K");
+});
+test('it returns 2K for 1905', function(assert) {
+  let result = niceNumber([1905]);
+  assert.equal(result, "2K");
+});
+test('it returns 42K for 42134', function(assert) {
+  let result = niceNumber([42123]);
+  assert.equal(result, "42K");
+});
