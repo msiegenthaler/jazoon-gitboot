3621834fb23abf2f1b4164a2bd65b6ce46369c96
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2016-04-07 15:50:31 +0200

    More information about the repositories.

--- app/models/repository.js
+++ app/models/repository.js
@@ -3,5 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr(),
   description: DS.attr(),
+  homepage: DS.attr(),
+  'stargazers-count': DS.attr(),
   organization: DS.belongsTo('organization')
 });
