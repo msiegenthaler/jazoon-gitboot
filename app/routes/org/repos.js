4f1f399d5d4d71efa19fe3d0ce0d3430f9971935
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2016-04-07 14:25:17 +0200

    Load the list of repos from the API.

--- app/routes/org/repos.js
+++ app/routes/org/repos.js
@@ -2,12 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return [{
-      id: 'emberjs'
-    }, {
-      id: 'data'
-    }, {
-      id: 'emberjs.github.io'
-    }];
+    const org = this.modelFor('org').id;
+    return fetch(`https://emberconf-state-api.herokuapp.com/api/organizations/${org}/repositories`)
+      .then(d => d.json())
+      .then(json => {
+        return json.data.map(item => {
+          return {
+            id: item.id,
+            name: item.attributes.name
+          };
+        });
+      });
   }
 });
