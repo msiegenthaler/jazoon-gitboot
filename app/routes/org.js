8f83ddc75be52e8af84a9e8d23b132bbf7eab57f
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2016-04-07 14:19:48 +0200

    Load the org as part of the route.

--- app/routes/org.js
+++ app/routes/org.js
@@ -1,9 +1,14 @@
-import Ember from 'ember';
+import Ember from "ember";
 
 export default Ember.Route.extend({
-  model() {
-    return {
-      id: 'Ember.js'
-    };
+  model(params) {
+    return fetch(`https://emberconf-state-api.herokuapp.com/api/organizations/${params.id}`)
+      .then(d => d.json())
+      .then(json => {
+        return {
+          id: json.data.id,
+          name: json.data.attributes.name
+        };
+      });
   }
 });
